fe7bdd0e4fd331208c8c06cbe763cfbd4d07a1d3
Jackson Perry <jackperry2187@gmail.com>
2021-11-16 06:28:28 -0500

    setup + users model

--- server/routes/API_Routes.js
+++ server/routes/API_Routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const KittenData = require("../data/controllers/test");
+const UserData = require('../data/controllers/user');
 
 router.get("/", async (req, res) => {
   try {
-    let dataDB = await KittenData.create();
+    let dataDB = await UserData.create();
+
     // console.log(dataDB);
-    res.send("WELCOME " + dataDB[0].name);
+    res.send("WELCOME " + dataDB[0].username);
   } catch (e) {
     console.log(e);
   }
