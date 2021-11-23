f6d42975ff0ca32b96495f6e783bf1d2f5248cb6
Jackson Perry <jackperry2187@gmail.com>
2021-11-23 01:59:43 -0500

    prettier edits + Schedules functions

--- server/data/controllers/user.js
+++ server/data/controllers/user.js
@@ -7,90 +7,81 @@ const User = require("../models/users");
  * @param {String} email User email also for account identification. Should also be unique.
  * @returns the created object if created
  */
-const create = async(username, email) => {
+const create = async (username, email) => {
   try {
     const created = new User({
       username: username,
       email: email,
       schedules: [],
-      admin: false
+      admin: false,
     });
     let added = await created.save();
-    const user = await User.find({ username: username , email: email }).exec();
-    if(user.length > 0) {
+    const user = await User.find({ username: username, email: email }).exec();
+    if (user.length > 0) {
       return user[0];
-    }
-    else throw new Error("User not created!");
-  }
-  catch(err) {
-    if(err.errors) {
-      if(err.errors.username) {
-          if (err.errors.username.kind === "mongoose-unique-validator") {
-            throw new Error("ERROR: Username must be unique");
-          }
+    } else throw new Error("User not created!");
+  } catch (err) {
+    if (err.errors) {
+      if (err.errors.username) {
+        if (err.errors.username.kind === "mongoose-unique-validator") {
+          throw new Error("ERROR: Username must be unique");
+        }
       }
-      if(err.errors.email) {
-          if (err.errors.email.kind === "mongoose-unique-validator") {
-            throw new Error("ERROR: Email must be unique");
-          }
+      if (err.errors.email) {
+        if (err.errors.email.kind === "mongoose-unique-validator") {
+          throw new Error("ERROR: Email must be unique");
+        }
       }
-    }
-    else throw new Error(err);
+    } else throw new Error(err);
   }
-}
+};
 
 /**
  * Finds a user in the database using a username
  * @param {String} username The username of the user.
  * @returns the user object, or null if not found
  */
-const readByUsername = async(username) => {
+const readByUsername = async (username) => {
   try {
     const user = await User.find({ username: username }).exec();
-    if(user.length > 0) {
+    if (user.length > 0) {
       return user[0];
-    }
-    else return null;
-  }
-  catch(err) {
+    } else return null;
+  } catch (err) {
     throw new Error(err);
   }
-}
+};
 
 /**
  * Finds a user in the database using an email
  * @param {String} email The email of the user.
  * @returns the user object, or null if not found
  */
-const readByEmail = async(email) => {
+const readByEmail = async (email) => {
   try {
     const user = await User.find({ email: email }).exec();
-    if(user.length > 0) {
+    if (user.length > 0) {
       return user[0];
-    }
-    else return null;
-  }
-  catch(err) {
+    } else return null;
+  } catch (err) {
     throw new Error(err);
   }
-}
+};
 
 /**
  * Finds all of the users in the database
  * @returns an array of user objects, or null if there are no users
  */
-const readAll = async() => {
+const readAll = async () => {
   try {
     const users = await User.find({}).exec();
-    if(users.length > 0) {
+    if (users.length > 0) {
       return users;
-    }
-    else return null;
-  }
-  catch(err) {
+    } else return null;
+  } catch (err) {
     throw new Error(err);
   }
-}
+};
 
 /**
  * Adds a scheduleId to the list of schedules of a user with a specific username
@@ -98,18 +89,20 @@ const readAll = async() => {
  * @param {String} scheduleId A stringified ObjectId of a Schedule.
  * @returns true if the schedule is added to the array, false if not
  */
-const addSchedule = async(username, scheduleId) => {
+const addSchedule = async (username, scheduleId) => {
   try {
-    let updated = await User.updateOne({ username: username }, { $push: { schedules: scheduleId } }).exec();
-    if(updated.modifiedCount == 1) {
+    let updated = await User.updateOne(
+      { username: username },
+      { $addToSet: { schedules: scheduleId } }
+    ).exec();
+    if (updated.modifiedCount == 1) {
       return true;
     }
     return false;
-  }
-  catch(err) {
+  } catch (err) {
     throw new Error(err);
   }
-}
+};
 
 /**
  * Removes a scheduleId from the list of schedules of a user with a specific username
@@ -117,36 +110,37 @@ const addSchedule = async(username, scheduleId) => {
  * @param {String} scheduleId A stringified ObjectId of a Schedule.
  * @returns true if the schedule is remove from the array, false if not
  */
-const removeSchedule = async(username, scheduleId) => {
+const removeSchedule = async (username, scheduleId) => {
   try {
-    let updated = await User.updateOne({ username: username }, { $pullAll: { schedules: [scheduleId] } }).exec();
-    if(updated.modifiedCount == 1) {
+    let updated = await User.updateOne(
+      { username: username },
+      { $pullAll: { schedules: [scheduleId] } }
+    ).exec();
+    if (updated.modifiedCount == 1) {
       return true;
     }
     return false;
-  }
-  catch(err) {
+  } catch (err) {
     throw new Error(err);
   }
-}
+};
 
 /**
  * Removes a user from the database
  * @param {String} username The username of the user.
  * @returns true if the user is removed, false if not
  */
-const remove = async(username) => {
+const remove = async (username) => {
   try {
     let removed = await User.deleteOne({ username: username }).exec();
-    if(removed.deletedCount == 1) {
+    if (removed.deletedCount == 1) {
       return true;
     }
     return false;
-  }
-  catch(err) {
+  } catch (err) {
     throw new Error(err);
   }
-}
+};
 
 module.exports = {
   description: "User functions",
@@ -156,5 +150,5 @@ module.exports = {
   readAll,
   addSchedule,
   removeSchedule,
-  remove
-}
+  remove,
+};
--- server/routes/API_Routes.js
+++ server/routes/API_Routes.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const UserData = require('../data/controllers/user');
-const ScheduleData = require('../data/controllers/schedules');
-const ClassData = require('../data/controllers/classes');
+const UserData = require("../data/controllers/user");
+const ScheduleData = require("../data/controllers/schedules");
+const ClassData = require("../data/controllers/classes");
 
 router.get("/", async (req, res) => {
   try {
+    //USER CONTROLLER TESTING
     //let data = await UserData.create("jperry2002", "jackperry100000@gmail.com");
     //let data = await UserData.readByUsername("jperry2002");
     //let data = await UserData.readByEmail("jackperry100000@gmail.com");
     //let data = await UserData.readAll();
     //let data = await UserData.addSchedule("jperry2002", "619be419da626506f45ce43b");
     //let data = await UserData.removeSchedule("jperry2002", "619be419da626506f45ce43b");
-    let data = await UserData.remove("jperry200020");
+    //let data = await UserData.remove("jperry200020");
+
+    //SCHEDULE CONTROLLER TESTING
+    //let data = await ScheduleData.create("Schedule 1", "2022 Spring Semester (01/18/2022-05/18/2022)", "6b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310");
+    //let data = await ScheduleData.readById("619c8f87dcbc544d65e0fec7");
+    //let data = await ScheduleData.readByUser("6b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310");
+    //let data = await ScheduleData.addClass("619c8f87dcbc544d65e0fec7", "6b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310");
+    //let data = await ScheduleData.removeClass("619c8f87dcbc544d65e0fec7", "6b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310");
+    //let data = await ScheduleData.updateName("619c8f87dcbc544d65e0fec7", "Schedule 18");
+    //let data = await ScheduleData.remove("619c8f87dcbc544d65e0fec7");
 
     console.log(data);
     res.send(data);
