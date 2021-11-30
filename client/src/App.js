7789435820406c33302f14e5085bb05a875e7490
Mrunal Salunke <mrunal.salunke.2018@gmail.com>
2021-11-30 16:30:10 -0500

    Basic layout of user profile page

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-          <Route exact path="/userProfile" component = {userProfile } />
+          <Route exact path="/userProfile" component = { userProfile } />
       </Router>
     </div>
   );
