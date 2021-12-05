0793ab775d6ae93382213098543133e81035e26c
Preet Dabhi <preetdabhi14@gmail.com>
2021-12-04 20:38:08 -0500

    made small changes to schedule page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
 import SchedulesPage from "./components/SchedulesPage";
-import Error from "./components/Error";
 import React from "react";
 import "./App.css";
-import ProfilePage from "./components/ProfilePage";
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				<Switch>
-					<Route exact path="/" component={Home} />
 					<Route exact path="/schedulespage" component={SchedulesPage} />
-					<Route exact path="/profilepage" component={ProfilePage} />
-					<Route exact path="*" component={Error} />
 				</Switch>
 			</div>
 		</Router>
