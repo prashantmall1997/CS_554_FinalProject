3c5f52c6e06d771db3d10cbed42d85a90b99bd65
Prashant Mall <prashantmall1997@gmail.com>
2021-12-06 20:21:56 -0500

    Modify for heroku

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ const constructorMethod = (app) => {
   app.use("/schedules", schedules);
   app.use("/users", users);
 
-  app.use("*", (req, res) => {
-    res.status(404).json({ error: "Not found" });
-  });
+  // app.use("*", (req, res) => {
+  //   res.status(404).json({ error: "Not found" });
+  // });
 };
 
 module.exports = constructorMethod;
