25363905f11961c6ad5b1d1028d3d8e6b3f986a0
shimmelr <shimmelr@stevens.edu>
2021-12-12 14:40:45 -0500

    adding package-lock to gitignore and removing admin.js to reduce confusion

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Admin from "./components/Admin";
 import CreateSchedule from "./components/CreateSchedule";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -8,7 +7,6 @@ function App() {
     <Router>
         <div className="App">
             <div className="App-body">
-                <Route exact path="/admin" component={Admin} />
                 <Route exact path="/createschedule" component={CreateSchedule} />
             </div>
         </div>
